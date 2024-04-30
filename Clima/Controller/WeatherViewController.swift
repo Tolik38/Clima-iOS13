38a9efc19ab1db257a56e959748e1e10c91fe272
Tolik <none>
2024-04-29 17:59:50 -0700

    first commit

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,60 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var weatherManager = WeatherManager()
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad() 
         // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
     }
-
+    
+    
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true) // close keyboard
+        print(searchTextField.text ?? "empty")
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true) // close keyboard
+        print (searchTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            print("type something")
+            return false
+        }
+            
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = searchTextField.text{
+            weatherManager.fetchWeather(cityName: city)
+        }
+        //Use searchTextField.text to get the weather for that city.
+        searchTextField.text = ""  // clear texfield
+    }
+    
+    
+    @IBAction func exitButton(_ sender: UIButton) {
+        exit(0)
+    }
+    
+    
 
 }
 
---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,47 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Admin on 4/27/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=2279b840d390cb60585367de278934b8&units=metric"
+    
+    func fetchWeather (cityName: String){
+        let urlSting = "\(weatherURL)&q=\(cityName)"
+        self.performRequest(urlString: urlSting)
+    }
+    
+    func performRequest (urlString: String) {
+        
+        //        1. Create a URL
+        if let url = URL(string: urlString){
+            //        2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            //        3. Give the session a Task
+            //let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil{
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data{
+                    parseJSON(weatherData: <#T##Data#>)
+                }
+            }
+            //        4.Start the task
+            task.resume()
+        }
+    }
+    func parseJSON(weatherData: Data){
+        
+    }
+    
+}
+
